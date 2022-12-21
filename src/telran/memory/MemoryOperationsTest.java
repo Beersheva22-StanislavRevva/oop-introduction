ce26b4488398b55a32ea65b9fa14a033813f79cb
stanislav-revva <nivadrive1@gmail.com>
2022-12-21 18:35:40 +0200

    commit 21-12-2022

--- src/telran/memory/MemoryOperationsTest.java
+++ src/telran/memory/MemoryOperationsTest.java
@@ -3,11 +3,14 @@ package telran.memory;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
 class MemoryOperationsTest {
+private static final long MGB = 1024 * 1024;
 byte ar[];
 
 	@Test
+	@Disabled
 	void maxMemoryTest() {
 		int maxMemory = MemoryOperations.getMaxAvaibleMemory();
 		ar = new byte[maxMemory];
@@ -22,5 +25,13 @@ byte ar[];
 		assertTrue(flException);
 		
 	}
+	@Test
+	void standardMemoryMethods() {
+		Runtime runtime = Runtime.getRuntime();
+		System.out.printf("Maximal memory JVM may require from OS: %d,"
+				+ " current total JVM memory:% d, current free JVM memory: %d ",
+				runtime.maxMemory() / MGB, runtime.totalMemory() / MGB, runtime.freeMemory() /MGB);
+	}
+
 
 }
